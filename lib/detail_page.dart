7c14cd826e1f8c21f5061cd09c3a81c6fd79efec
desphyr <achmad.abddillah23@student.uisi.ac.id>
2025-12-10 11:17:03 +0700

    feat: menyesuaikan styling pada font judul AppBar

--- lib/detail_page.dart
+++ lib/detail_page.dart
@@ -14,7 +14,14 @@ class _DetailPageState extends State<DetailPage> {
       backgroundColor: Color(0xFFF5FBFB),
       appBar: AppBar(
         backgroundColor: Color(0xFFF5FBFB),
-        title: Text('Venusaur'),
+        title: Text(
+            'Venusaur',
+            style: TextStyle(
+            color: Color(0xFF232447),
+            fontWeight: FontWeight.bold,
+            fontSize: 29,
+          ),
+        ),
       ),
       body: Container(),
     );
